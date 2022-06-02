d7c6a529d80d66daffdb467c57a877c3ccc649e0
martin835 <64438132+martin835@users.noreply.github.com>
2022-06-02 11:57:59 +0200

    chat with socket IO started to be implemted

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import app from "./app.js";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+let onlineUsers = [];
+// Server connection
 
 const port = process.env.PORT || 3001;
 
@@ -8,13 +13,35 @@ if (!process.env.MONGO_CONNECTION) {
   throw new Error("No Mongo url");
 }
 
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  origin: "https://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true,
+});
+
+io.on("connection", async (socket) => {
+  console.log("🔛 SOCKET ID: ", socket.id);
+  console.log(
+    "🤝 HANDSHAKE - is there a token in the headers ? : ",
+    socket.handshake
+  );
+
+  socket.emit("welcome");
+
+  socket.on("disconnect", () => {
+    console.log(`❌ disconnected`);
+    console.log(" 📻 ONLINE USERS: ", onlineUsers);
+  });
+});
+
 mongoose.connect(process.env.MONGO_CONNECTION);
 
 mongoose.connection.on("connected", () => {
   console.log("👌 Connected to Mongo!");
 
-  app.listen(port, () => {
+  httpServer.listen(port, () => {
     console.table(listEndpoints(app));
-    console.log(`🚀 Server listening on port ${port}`);
+    console.log(`🟢 Server listening on port ${port} 🚀 `);
   });
 });
