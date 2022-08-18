fccee06adfeb12a878bf9bd0e674dc3e1d347a64
martin835 <64438132+martin835@users.noreply.github.com>
2022-08-18 16:19:07 +0200

    overview for services fixed

--- src/services/routers/basicReview-router.js
+++ src/services/routers/basicReview-router.js
@@ -15,34 +15,18 @@ basicReviewRouter.get("/", async (req, res, next) => {
   console.log("QUERY-TO-MONGO: ", q2m(req.query));
   try {
     const mongoQuery = q2m(req.query);
+
     if (mongoQuery.criteria.providerId) {
       console.log("I carry provider ID");
-    } else if (mongoQuery.criteria.serviceId) {
-      {
-        console.log("I carry service ID");
-      }
-    } else {
-      {
-        console.log("I carry no ID");
-      }
-    }
+      const reviews = await BasicReviewModel.find({
+        provider: mongoQuery.criteria.providerId,
+      })
+        .limit(mongoQuery.options.limit || 6)
+        .skip(mongoQuery.options.skip || 0)
+        .sort({ createdAt: -1 })
+        .populate({ path: "user", select: "_id name surname avatar" })
+        .populate({ path: "service", select: "_id type name" });
 
-    const reviews = await BasicReviewModel.find(
-      mongoQuery.criteria.providerId
-        ? {
-            provider: mongoQuery.criteria.providerId,
-          }
-        : {}
-    )
-      .limit(mongoQuery.options.limit || 6)
-      .skip(mongoQuery.options.skip || 0)
-      .sort({ createdAt: -1 })
-      .populate({ path: "user", select: "_id name surname avatar" })
-      .populate({ path: "service", select: "_id type name" });
-
-    //if request is for specific client center calculate average rating based on available reviews.
-
-    if (mongoQuery.criteria.providerId) {
       const reviewsToCalcStats = await BasicReviewModel.find({
         provider: mongoQuery.criteria.providerId,
       });
@@ -92,7 +76,77 @@ basicReviewRouter.get("/", async (req, res, next) => {
         },
         reviews: reviews,
       });
+    } else if (mongoQuery.criteria.serviceId) {
+      console.log("I carry service ID");
+      const reviews = await BasicReviewModel.find({
+        service: { _id: mongoQuery.criteria.serviceId },
+      })
+        .limit(mongoQuery.options.limit || 6)
+        .skip(mongoQuery.options.skip || 0)
+        .sort({ createdAt: -1 })
+        .populate({ path: "user", select: "_id name surname avatar" })
+        .populate({ path: "service", select: "_id type name" });
+
+      const reviewsToCalcStats = await BasicReviewModel.find({
+        service: { _id: mongoQuery.criteria.serviceId },
+      });
+
+      console.log("reviews for service: ", reviewsToCalcStats);
+
+      let sumOfAllReviews = 0;
+      for (let i = 0; i < reviewsToCalcStats.length; i++) {
+        sumOfAllReviews += reviewsToCalcStats[i].rating;
+      }
+
+      let weight5 =
+        (reviewsToCalcStats.filter((review) => review.rating === 5).length /
+          reviewsToCalcStats.length) *
+        100;
+      let weight4 =
+        (reviewsToCalcStats.filter((review) => review.rating === 4).length /
+          reviewsToCalcStats.length) *
+        100;
+      let weight3 =
+        (reviewsToCalcStats.filter((review) => review.rating === 3).length /
+          reviewsToCalcStats.length) *
+        100;
+      let weight2 =
+        (reviewsToCalcStats.filter((review) => review.rating === 2).length /
+          reviewsToCalcStats.length) *
+        100;
+      let weight1 =
+        (reviewsToCalcStats.filter((review) => review.rating === 1).length /
+          reviewsToCalcStats.length) *
+        100;
+
+      let avgRating =
+        Math.round((sumOfAllReviews / reviewsToCalcStats.length) * 10) / 10;
+      // console.log(
+      //   "AVERAGE RATING: ",
+      //   sumOfAllReviews / reviewsToCalcStats.length
+      // );
+
+      res.send({
+        avgRating: avgRating,
+        reviewsCount: reviewsToCalcStats.length,
+        weights: {
+          weight5: weight5,
+          weight4: weight4,
+          weight3: weight3,
+          weight2: weight2,
+          weight1: weight1,
+        },
+        reviews: reviews,
+      });
     } else {
+      console.log("I carry no ID");
+      const reviews = await BasicReviewModel.find({})
+        .limit(mongoQuery.options.limit || 6)
+        .skip(mongoQuery.options.skip || 0)
+        .sort({ createdAt: -1 })
+        .populate({ path: "user", select: "_id name surname avatar" })
+        .populate({ path: "service", select: "_id type name" });
+
       res.send(reviews);
     }
   } catch (error) {
